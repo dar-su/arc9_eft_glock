46de4461f62a2288477e26abd00977ab80569c7f
Darsu <95997959+dar-su@users.noreply.github.com>
2023-04-14 19:52:08 +0700

    spread bonus

--- lua/weapons/arc9_eft_glock17.lua
+++ lua/weapons/arc9_eft_glock17.lua
@@ -183,7 +183,7 @@ SWEP.RecoilKickDamping = 10 -- Camera recoil damping
 -------------------------- SPREAD
 
 SWEP.Spread = 0.003
-SWEP.SpreadAddHipFire = 0.02
+SWEP.SpreadAddHipFire = 0.03
 SWEP.SpreadMultMove = 1.5
 SWEP.SpreadAddMove = 0.015
 
@@ -498,11 +498,10 @@ SWEP.Attachments = {
 
 SWEP.EFTErgo = 90
 if ARC9EFTBASE then
-    SWEP.AimDownSightsTimeHook = ARC9EFT.ErgoHook
-    if ARC9EFT.ErgoBreathHook then
-        SWEP.HoldBreathTimeHook = ARC9EFT.ErgoBreathHook
-        SWEP.HookP_TranslateSound = ARC9EFT.ErgoAdsVolume
-    end
+    SWEP.AimDownSightsTimeHook = ARC9EFT.ErgoHook or nil
+    SWEP.HoldBreathTimeHook = ARC9EFT.ErgoBreathHook or nil
+    SWEP.HookP_TranslateSound = ARC9EFT.ErgoAdsVolume or nil
+    SWEP.SpreadHook = ARC9EFT.SpreadBonus or nil
 else
     print("Dum! install arc9 eft shared!!!!!!!!!!!!!!")
 end
